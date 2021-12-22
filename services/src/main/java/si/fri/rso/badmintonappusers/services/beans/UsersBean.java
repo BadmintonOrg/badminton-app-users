fe225eac1f871aade1ac46f5864bc3e2d7b29291
Cesarmarti <mresman28@gmail.com>
2021-12-22 12:11:47 +0100

    Added fault tolerance

--- services/src/main/java/si/fri/rso/badmintonappusers/services/beans/UsersBean.java
+++ services/src/main/java/si/fri/rso/badmintonappusers/services/beans/UsersBean.java
@@ -1,15 +1,22 @@
 package si.fri.rso.badmintonappusers.services.beans;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import com.kumuluz.ee.rest.utils.JPAUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import si.fri.rso.badmintonappusers.lib.Organization;
 import si.fri.rso.badmintonappusers.lib.User;
 import si.fri.rso.badmintonappusers.models.converters.UserConverter;
@@ -23,10 +30,15 @@ import javax.persistence.EntityManager;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.io.IOException;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -45,6 +57,8 @@ public class UsersBean {
     private HttpClient httpClient;
     private ObjectMapper objectMapper;
 
+    private Client httpClient2;
+
     @Inject
     @DiscoverService(value = "badmiton-app-organizations-service", version = "1.0.0", environment = "dev")
     private Optional<WebTarget> target;
@@ -55,6 +69,7 @@ public class UsersBean {
     @PostConstruct
     private void init() {
         httpClient = HttpClientBuilder.create().build();
+        httpClient2 = ClientBuilder.newClient();
         objectMapper = new ObjectMapper();
     }
 
@@ -67,6 +82,9 @@ public class UsersBean {
                 .map(UserConverter::toDto).collect(Collectors.toList());
     }
 
+    @Inject
+    private UsersBean userBeanProxy;
+
     public User getUser(Integer id) {
 
         UsersEntity usersEntity = em.find(UsersEntity.class, id);
@@ -78,15 +96,50 @@ public class UsersBean {
         User comm = UserConverter.toDto(usersEntity);
 
         log.log(Level.INFO,String.valueOf(restProperties.getUserDiscovery()));
-        if(target.isPresent()&&restProperties.getUserDiscovery()){
-            Organization org = getOrganizationFromService(comm.getOrganization());
-            if(org!=null)
-                comm.setOrganizationObj(org);
+        if(restProperties.getUserDiscovery()){
+            Organization org = userBeanProxy.getOrganizationFromService(comm.getOrganization());
+            comm.setOrganizationObj(org);
         }
 
         return comm;
     }
 
+    @Timeout(value=500)
+    @Retry(maxRetries = 2)
+    @CircuitBreaker(requestVolumeThreshold = 3)
+    @Fallback(fallbackMethod = "getOrgFallback")
+    public Organization getOrganizationFromService(Integer id){
+        WebTarget service = target.get().path("v1/organizations/"+id);
+        log.log(Level.INFO,String.valueOf(service.getUri()));
+        try {
+            return httpClient2
+                    .target(service.getUri())
+                    .request().get(new GenericType<Organization>() {
+                    });
+        }
+        catch (WebApplicationException | ProcessingException e) {
+            log.severe(e.getMessage());
+            throw new InternalServerErrorException(e);
+        }
+    }
+
+
+    /*@Timeout(value=500)
+    @Fallback(fallbackMethod = "getOrgFallback")
+    public Organization getOrganizationFromService(Integer id){
+        WebTarget service = target.get().path("v1/organizations/1");
+        try {
+            return httpClient2
+                    .target(service.getUri())
+                    .request().get(new GenericType<Organization>() {
+                    });
+        }
+        catch (WebApplicationException | ProcessingException e) {
+            log.severe(e.getMessage());
+            throw new InternalServerErrorException(e);
+        }
+    }*/
+    /*
     public Organization getOrganizationFromService(Integer id){
         WebTarget service = target.get().path("v1/organizations");
         log.log(Level.INFO,String.valueOf(service.getUri()));
@@ -112,8 +165,16 @@ public class UsersBean {
     }
 
 
+
+    * */
+
+
     public Organization getOrgFallback(Integer id) {
-        return null;
+        log.log(Level.INFO,"Fallback triggered");
+        Organization org = new Organization();
+        org.setName("Fallback organization");
+        org.setOrganizationId(-1);
+        return org;
     }
 
     public User createUser(User comm) {
